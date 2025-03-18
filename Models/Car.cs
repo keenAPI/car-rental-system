fcfd14d67c6ea7a2bc3c3671ac1503059e993830
Keenan Fourie <Keenan.Fourie@redacademy.co.za>
2025-03-18 11:48:21 +0200

    Created the Car Model

---
+++ Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Rental_System.Models
+{
+    class Car
+    {
+        public int CarID { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public decimal PricePerDay { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
