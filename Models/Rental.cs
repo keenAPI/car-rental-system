1bf38be9cd1bd2359231489e18fc20c0c038f5f4
Keenan Fourie <Keenan.Fourie@redacademy.co.za>
2025-04-30 11:44:32 +0200

    Added the Rental class to my program

---
+++ Models/Rental.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Rental_System.Models
+{
+    class Rental
+    {
+        public int RentalID { get; set; }
+        public int UserID { get; set; }
+        public int CarID { get; set; }
+        public DateTime RentalDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+    }
+}
