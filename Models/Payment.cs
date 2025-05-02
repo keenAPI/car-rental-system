d790c016c20d0358566dadba03b25d52838ab7dd
Keenan Fourie <Keenan.Fourie@redacademy.co.za>
2025-05-02 10:15:04 +0200

    Added my Payment class to my Model

---
+++ Models/Payment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Rental_System.Models
+{
+    class Payment
+    {
+        public int PaymentID { get; set; }
+        public int RentalID { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime DatePaid { get; set; }
+    }
+}
